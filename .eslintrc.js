36448d58dae0898d7c95fe611801300a2c43e18f
extracold <extracold1209@gmail.com>
2023-04-23 17:55:20 +0900

    feat: msw 및 설정 추가, 테스트용 mock API 추가

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,27 +1,27 @@
 module.exports = {
-    env: {
-        browser: true,
-        es2021: true,
-        node: true
-    },
-    extends: [
-        'eslint:recommended',
-        'plugin:react/recommended'
-    ],
-    overrides: [
-    ],
-    parserOptions: {
-        ecmaVersion: 'latest',
-        sourceType: 'module'
-    },
-    plugins: [
-        'react'
-    ],
-    rules: {
-        indent: ['error', 4],
-        quotes: ['error','single'],
-        semi: ['error','always'],
-        'linebreak-style': ['error','unix'],
-        'quote-props': ['error', 'as-needed']
-    }
+  env: {
+    browser: true,
+    es2021: true,
+    node: true
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended'
+  ],
+  overrides: [
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module'
+  },
+  plugins: [
+    'react'
+  ],
+  rules: {
+    indent: ['error', 2],
+    quotes: ['error','single'],
+    semi: ['error','always'],
+    'linebreak-style': ['error','unix'],
+    'quote-props': ['error', 'as-needed']
+  }
 };
