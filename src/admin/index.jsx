87652c302798fc38bd6463bfce03eb745dbd42bb
extracold <extracold1209@gmail.com>
2023-05-13 19:43:03 +0900

    test: lint-staged pre-commit 테스트

--- src/admin/index.jsx
+++ src/admin/index.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import AdminPage from './adminPage';
 
+console.log('test');
 if (process.env.NODE_ENV === 'development') {
   const {worker} = require('../mocks/browser');
   worker.start();
