3be7020904a3dd2f17cb285b04f1dc3ce9a9b15a
jooyeon <dldbsrud0925@gmail.com>
2023-05-20 23:47:37 +0900

    feat: msw GET POST /login API 추가

--- src/mocks/browser.js
+++ src/mocks/browser.js
@@ -1,5 +1,6 @@
 import { setupWorker } from 'msw';
 import { handlers } from './handlers';
 import {domainApiHandlers} from './admin';
+import {handler as loginHandler} from './login';
 // This configures a Service Worker with the given request handlers.
-export const worker = setupWorker(...handlers, ...domainApiHandlers);
+export const worker = setupWorker(...handlers, ...domainApiHandlers, ...loginHandler);
