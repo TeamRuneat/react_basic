2c21e7d19ed9f79ada057f85e43927e0bf344131
jooyeon <dldbsrud0925@gmail.com>
2023-06-05 22:19:15 +0900

    feat: 메인 홈 페이지 구현

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,14 +1,6 @@
-import React, {useEffect} from 'react';
+import React from 'react';
+import Shop from './Shop';
 
 export default function Home(){
-  useEffect(() => {
-    fetch('/hello').then((response) => {
-      if (response.ok) {
-        response.json().then(console.log).catch(console.error);
-      } else {
-        alert('msw not ok');
-      }
-    });
-  }, []);
-  return <div>Hello World!</div>;
+  return <Shop/>;
 }
---
+++ src/pages/Shop.jsx
@@ -0,0 +1,65 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import ShopList from './ShopList';
+import Button from '../components/Button';
+import Searchbox from '../components/Searchbox';
+
+const shopOptions = ['음식', '가격', '태그'];
+const sortOptions = ['거리순', '별점순', '최신순'];
+
+export default function RestaurantList(){
+  const [lists, setLists] = useState([]);
+  const [foodSelected, setFoodSelected] = useState(shopOptions[0]);
+  const [sortSelected, setSortSelected] = useState(sortOptions[0]);
+
+  const SELECT_CLASS = 'py-1 pl-2 pr-6 cursor-pointer utline-none';
+
+  useEffect(() => {
+    const getLists = async () => {
+      try{
+        const res = await axios.get('http://ec2-43-200-176-108.ap-northeast-2.compute.amazonaws.com/shop-list')
+        setLists(res.data);
+      } catch(error) {
+        console.log(error)
+      }
+    }
+    getLists();
+  }, []);
+
+  return (
+    <div>
+      <Searchbox />
+      <div className='relative'>
+        <div className='ml-8'>
+          <select
+            id='shopSelect'
+            className={SELECT_CLASS}
+            value={foodSelected}
+            onChange={(e) => setFoodSelected(e.target.value)}
+          >
+            {shopOptions.map((option, index) => (
+              <option key={index}>{option}</option>
+            ))}
+          </select>
+          <select
+            id='sortSelect'
+            className={SELECT_CLASS}
+            value={sortSelected}
+            onChange={(e) => setSortSelected(e.target.value)}
+          >
+            {sortOptions.map((option, index) => (
+              <option key={index} >{option}</option>
+            ))}
+          </select>
+        </div>
+        <div className='absolute top-0 right-8'>
+          <Button text={'공유하기'} variant='default' />
+          <Button text={'랜덤뽑기'} variant='border' />
+        </div>
+      </div>
+      <ul className='grid grid-cols-1 md:grid-cols-4 lg-grid-cols-4 gap-10 p-10'>
+        {lists?.map((item) => <ShopList key={item.id} item={item} />)}
+      </ul>
+    </div>
+  )
+}
