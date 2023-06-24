d4da8b562740c4d93cb0f4e46d0f70b9f652e790
jooyeon <dldbsrud0925@gmail.com>
2023-06-24 21:19:20 +0900

    fix: webpack 파일로더 수정

--- webpack.config.js
+++ webpack.config.js
@@ -31,7 +31,7 @@ module.exports = {
         ],
       },
       {
-        test: /\.(png|jpe?g|gif)$/i,
+        test: /\.(png|jpe?g|gif|svg)$/i,
         loader: 'file-loader',
         options: {
           name: '[name].[ext]',
