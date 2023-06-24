1c571df9259f600fdd6beb1ca0511035110a9511
jooyeon <dldbsrud0925@gmail.com>
2023-06-24 21:17:07 +0900

    feat: tailwind config 폰트 설정 및 css 폰트 추가

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,23 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        NanumSquare: ['NanumSquare', 'sans-serif'],
+        BMJua: ['BMJua', 'sans-serif'],
+      },
+      fontSize: {
+        10: '0.625rem',
+        11: '0.688rem',
+        12: '0.75rem',
+        14: '0.875rem',
+        15: '0.938rem',
+        16: '1rem',
+        18: '1.125rem',
+        20: '1.25rem',
+        24: '1.5rem',
+      },
+    },
   },
   plugins: [],
 };
