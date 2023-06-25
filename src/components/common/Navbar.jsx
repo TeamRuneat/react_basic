29f74999f2a46896be969922b70f3eafe9f8b796
jooyeon <dldbsrud0925@gmail.com>
2023-06-25 19:33:49 +0900

    fix: navbar불필요한 코드 삭제 및 css 수정

--- src/components/common/Navbar.jsx
+++ src/components/common/Navbar.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Link, useLocation } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import Button from './Button';
 import LoginModal from '../login/LoginModal';
 import logo from '../../../public/images/logo.png';
@@ -7,21 +7,16 @@ import logo from '../../../public/images/logo.png';
 export default function Navbar(){
   const {user} = useState();
   const [openModal, setOpenModal] = useState(false);
-  const location = useLocation();
-
-  if(location.pathname === '/login'){
-    return null;
-  }
 
   return(
     <header className='relative'>
-      <h1 className='px-10 py-7 text-2xl'>
+      <h1 className='px-10 py-7 text-2xl inline-block'>
         <Link to='/'>
           <img src={logo} alt='logo' className='w-[80px] h-[30]' />
         </Link>
       </h1>
-      <nav className='absolute top-10 right-12 font-semibold'>
-        {!user &&  <button onClick={() => setOpenModal(true)}>로그인</button>}
+      <nav className='absolute top-5 right-12 font-semibold'>
+        {!user &&  <button onClick={() => setOpenModal(true)} className='p-5'>로그인</button>}
         {openModal && <LoginModal onCloseModal={() => setOpenModal(false)} />}
         {user && <Button text={'로그아웃'}/>}
       </nav>
