7cd9d10d1f5d5f3afc547531a3ce0052ab279e8c
jooyeon <dldbsrud0925@gmail.com>
2023-07-15 01:32:16 +0900

    feat: react query 설치 및 추가

--- packages/client/src/App.jsx
+++ packages/client/src/App.jsx
@@ -1,12 +1,19 @@
 import React from 'react';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { Outlet } from 'react-router-dom';
 import Navbar from './components/common/Navbar';
+
+const queryClient = new QueryClient();
 
 const App = () => {
   return(
     <div>
-      <Navbar />
-      <Outlet />
+      <QueryClientProvider client={queryClient}>
+        <Navbar />
+        <Outlet />
+        <ReactQueryDevtools initialIsOpen={false} />
+      </QueryClientProvider>
     </div>
   );
 };
