7f1e37e420a0a8a3b87f29a7e7215679a4ea7009
jooyeon <dldbsrud0925@gmail.com>
2023-07-20 22:16:03 +0900

    fix: useGetUser usequery error handling

--- packages/client/src/hooks/useGetUser.js
+++ packages/client/src/hooks/useGetUser.js
@@ -1,8 +1,17 @@
+import { useNavigate } from 'react-router-dom';
 import { useQuery } from '@tanstack/react-query';
 import { getUser } from '../apis/authApi';
 
 const useGetUser = () => {
-  return useQuery(['user'], () => getUser());
+  const navigate = useNavigate();
+  return useQuery(['user'], getUser, {
+    onError: (error) => {
+      const { response } = error;
+      if (response.status === 403) {
+        navigate('/');
+      }
+    },
+  });
 }
 
 export default useGetUser;
