3604b0257641fb2559045ff8bfd32d723c4d70ef
jooyeon <dldbsrud0925@gmail.com>
2023-07-20 22:14:38 +0900

    feat: react query 기본 설정 추가

--- packages/client/src/App.jsx
+++ packages/client/src/App.jsx
@@ -4,17 +4,22 @@ import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { Outlet } from 'react-router-dom';
 import Navbar from './components/common/Navbar';
 
-const queryClient = new QueryClient();
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      retry: false,
+      refetchOnWindowFocus: false,
+    },
+  },
+});
 
 const App = () => {
   return(
-    <div>
-      <QueryClientProvider client={queryClient}>
-        <Navbar />
-        <Outlet />
-        <ReactQueryDevtools initialIsOpen={false} />
-      </QueryClientProvider>
-    </div>
+    <QueryClientProvider client={queryClient}>
+      <Navbar />
+      <Outlet />
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
   );
 };
 
