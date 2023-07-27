950cdba5f907852a1045a5424bb25dbdec829401
jooyeon <dldbsrud0925@gmail.com>
2023-07-27 21:50:45 +0900

    fix: 로그인 관련 css 수정

--- packages/client/tailwind.config.js
+++ packages/client/tailwind.config.js
@@ -18,6 +18,35 @@ module.exports = {
         20: '1.25rem',
         24: '1.5rem',
       },
+      colors: {
+        brand: '#009389',
+        dim: '#eeeeee',
+      },
+      animation: {
+        bounce1: 'bounce 2s ease-in-out infinite ',
+        bounce2: 'bounce 2s 0.5s ease-in-out infinite ',
+        bounce3: 'bounce 2s 1s ease-in-out infinite ',
+        bounce4: 'slider 1s',
+      },
+      keyframes: {
+        bounce: {
+          '0%, 20%': { trasnsform: `translate(0,0)` },
+          '50%': {
+            transform: 'translateY(-150%)',
+          },
+          '85%, 100%': {
+            trasnsform: `translate(0,0)`,
+          },
+        },
+        slider: {
+          '0%': {
+            transform: 'translateY(-30%)',
+          },
+          '100%': {
+            transform: 'translateY(0)',
+          },
+        },
+      },
     },
   },
   plugins: [],
