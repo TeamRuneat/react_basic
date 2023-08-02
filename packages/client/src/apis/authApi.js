b9f8e8bfabb54b7a1fb04c4513d3edc4436c01ab
jooyeon <dldbsrud0925@gmail.com>
2023-08-02 23:07:42 +0900

    fix: 반복되는 API get 요청 공통으로 빼기

--- packages/client/src/apis/authApi.js
+++ packages/client/src/apis/authApi.js
@@ -2,14 +2,16 @@ import axios from 'axios';
 
 axios.defaults.withCredentials = true;
 
-export function loginCheck(){
-  return axios.get('api/auth/check').then((response) => response.data);
+const get = (url) => axios.get(url).then((response) => response.data);
+
+export function loginCheck() {
+  return get('api/auth/check');
 }
 
-export function getUser(){
-  return axios.get('/api/auth/user').then((response) => response.data);
+export function getUser() {
+  return get('/api/auth/user');
 }
 
-export function logout(){
-  return axios.get('api/auth/logout').then((response) => response.data);
+export function logout() {
+  return get('api/auth/logout');
 }
