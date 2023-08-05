abde0035149a3a446a4b6dfabee35d7fc7cc67c5
jooyeon <dldbsrud0925@gmail.com>
2023-08-06 00:54:07 +0900

    fix: navigate 삭제

--- packages/client/src/hooks/useLogout.js
+++ packages/client/src/hooks/useLogout.js
@@ -1,15 +1,12 @@
 import { useMutation, useQueryClient } from '@tanstack/react-query';
-import { useNavigate } from 'react-router-dom';
 import { logout } from '../apis/authApi';
 
 const useLogout = () => {
   const queryClient = useQueryClient();
-  const navigate = useNavigate();
   return useMutation(logout, {
     onSuccess: () => {
       queryClient.setQueryData(['user'], null);
       queryClient.setQueryData(['isLoggedIn'], false);
-      navigate('/');
     },
     onError: (error) => {
       const { response } = error;
