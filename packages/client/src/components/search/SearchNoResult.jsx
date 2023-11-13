8587fe0520539de987ce488da969b2dd107a21eb
jooyeon <dldbsrud0925@gmail.com>
2023-11-13 21:29:06 +0900

    feat: 검색 결과 없음 페이지 추가

--- packages/client/src/components/search/SearchNoResult.jsx
+++ packages/client/src/components/search/SearchNoResult.jsx
@@ -1,17 +1,20 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
+import SvgIcon from '../ui/SvgIcon';
+import common from '../../../public/icons/common.svg';
 
 export default function SearchNoResult() {
   const navigate = useNavigate();
   
   return(
     <section>
-      <div className='flex flex-col justify-center items-center h-[calc(100vh-300px)]'>
-        <p className='my-3 text-xl font-bold leading-6'>검색한 결과가 없습니다</p>
-        <p>검색한 식당이 런잇에 없다면? 등록하기 버튼을 통해 식당을 등록해주세요!</p>
+      <div className='flex flex-col justify-center items-center h-[calc(100vh-150px)]'>
+        <SvgIcon type={common} id='alert' width={80} height={80} />
+        <p className='my-10 mb-5 text-28 font-bold leading-7'>검색어에 대한 결과가 없습니다.</p>
+        <p className='text-24'>검색한 식당이 런잇에 없다면 등록하기 버튼을 통해 식당을 등록해주세요!</p>
         <button 
-          onClick={() => navigate('/')}
-          className='mt-3 py-3 px-4 bg-black text-white text-sm font-bold rounded-full'
+          onClick={() => navigate('/new')}
+          className='mt-20 w-[422px] h-20 text-26 rounded-xl border border-black'
         >
           등록하기
         </button>
